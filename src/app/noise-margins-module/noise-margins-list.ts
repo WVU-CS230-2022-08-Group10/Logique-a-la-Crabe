60353e496b9755ef849b24a5f3a9823f7638e1ed
tanner-mann <111549216+tanner-mann@users.noreply.github.com>
2022-12-14 18:55:11 -0500

    Noise margin questions

--- src/app/noise-margins-module/noise-margins-list.ts
+++ src/app/noise-margins-module/noise-margins-list.ts
@@ -39,38 +39,38 @@ export const noise_margins_practice_questions = [
 
 export const noise_margins_quiz_questions = [
     {
-        question: "Question 1: ",
-        correct_answer: "",
-        wrong_answer1: "",
-        wrong_answer2: "",
+        question: "Question 1: Gate A has a VIL of 1, a VIH of 3, a VOL of 1, and a VIL of 3. Gate B has a VIL of 1.5, a VIH of 2.5, a VOL of .5, and a VIL of 4. If gate A is driving gate B, what is the NMH?",
+        correct_answer: ".5",
+        wrong_answer1: "1",
+        wrong_answer2: "-.5",
+        wrong_answer3: "0"
+    },
+    {
+        question: "Question 2: Gate A has a VIL of 1, a VIH of 3, a VOL of 1, and a VIL of 3. Gate B has a VIL of 1.5, a VIH of 2.5, a VOL of .5, and a VIL of 4. If gate A is driving gate B, what is the NML?",
+        correct_answer: ".5",
+        wrong_answer1: "1",
+        wrong_answer2: "-.5",
+        wrong_answer3: "0"
+    },
+    {
+        question: "Question 3: Gate A has a VIL of 1, a VIH of 3, a VOL of 1, and a VIL of 3. Gate B has a VIL of 1.5, a VIH of 2.5, a VOL of .5, and a VIL of 4. Can gate A drive gate B?",
+        correct_answer: "Yes",
+        wrong_answer1: "No",
+        wrong_answer2: "There is no margin",
         wrong_answer3: "Not enough information"
     },
     {
-        question: "Question 2: ",
-        correct_answer: "",
-        wrong_answer1: "",
-        wrong_answer2: "",
+        question: "Question 4: Gate A has a VIL of 1, a VIH of 3, a VOL of 1, and a VIL of 3. Gate B has a VIL of 1.5, a VIH of 2.5, a VOL of .5, and a VIL of 4. Can gate B drive gate A?",
+        correct_answer: "Yes",
+        wrong_answer1: "No",
+        wrong_answer2: "There is no margin",
         wrong_answer3: "Not enough information"
     },
     {
-        question: "Question 3: ",
-        correct_answer: "",
-        wrong_answer1: "",
-        wrong_answer2: "",
-        wrong_answer3: "Not enough information"
-    },
-    {
-        question: "Question 4: ",
-        correct_answer: "",
-        wrong_answer1: "",
-        wrong_answer2: "",
-        wrong_answer3: "Not enough information"
-    },
-    {
-        question: "Question 5: ",
-        correct_answer: "",
-        wrong_answer1: "",
-        wrong_answer2: "",
+        question: "Question 5: Gate A has a VIL of 1, a VIH of 3, a VOL of 1, and a VIL of 3. Gate B has a VIL of 1.5, a VIH of 2.5, a VOL of .5, and a VIL of 4. Can gate B drive gate B?",
+        correct_answer: "Yes",
+        wrong_answer1: "No",
+        wrong_answer2: "There is no margin",
         wrong_answer3: "Not enough information"
     }
     
