f5835809a681b4ecfe09060d1a7d03f779492ca4
tanner-mann <111549216+tanner-mann@users.noreply.github.com>
2022-12-14 18:43:33 -0500

    Noise margin updates

--- src/app/noise-margins-module/noise-margins.component.spec.ts
+++ src/app/noise-margins-module/noise-margins.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NoiseModuleComponent } from './noise-margins.component';
 
-describe('TransistorModuleComponent', () => {
+describe('NoiseModuleComponent', () => {
   let component: NoiseModuleComponent;
   let fixture: ComponentFixture<NoiseModuleComponent>;
 
