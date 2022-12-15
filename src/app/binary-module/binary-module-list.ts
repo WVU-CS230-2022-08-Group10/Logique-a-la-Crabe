59c7f7cd031ce0e3c59244f6a24481faecff01a2
01ethanwright <01ethanwright@gmail.com>
2022-12-15 07:10:22 -0500

    binary module with added comments

--- src/app/binary-module/binary-module-list.ts
+++ src/app/binary-module/binary-module-list.ts
@@ -1,47 +1,78 @@
 export const binary_practice_questions = [
     {
-        question: "Question 1:",
-        correct_answer: ""
+        question: "Question 1: What is the binary number 0101 equal to?",
+        correct_answer: "5",
+        wrong_answer1: "7",
+        wrong_answer2: "3",
+        wrong_answer3: "4"
     },
     {
-        question: "Question 2:",
-        correct_answer: ""
+        question: "Question 2: What is the binary number 1111 equal to?",
+        correct_answer: "15",
+        wrong_answer1: "10",
+        wrong_answer2: "12",
+        wrong_answer3: "19"
     },
     {
-        question: "Question 3:",
-        correct_answer: ""
+        question: "Question 3: What is the sum of this binary addition? 010 + 001",
+        correct_answer: "011",
+        wrong_answer1: "100",
+        wrong_answer2: "010",
+        wrong_answer3: "001"
+
     },
     {
-        question: "Question 4:",
-        correct_answer: ""
+        question: "Question 4: What is the sum of this binary addition? 1010 + 0010",
+        correct_answer: "1100",
+        wrong_answer1: "1011",
+        wrong_answer2: "1110",
+        wrong_answer3: "1010"
     },
     {
-        question: "Question 5:",
-        correct_answer: ""
+        question: "Question 5: What is the sum of this binary subtraction in decimal in two's compliment? 111 + 001",
+        correct_answer: "1000",
+        wrong_answer1: "000",
+        wrong_answer2: "1110",
+        wrong_answer3: "1001"
     }
     
 ]
 
 export const binary_quiz_questions = [
     {
-        question: "Question 1: ",
-        correct_answer: ""
+        question: "Question 1: What is this number 9 in binary (unsigned)?",
+        correct_answer: "1001",
+        wrong_answer1: "1010",
+        wrong_answer2: "0101",
+        wrong_answer3: "0110"
     },
     {
-        question: "Question 2: ",
-        correct_answer: ""
+        question: "Question 2: What is the number 1100 (unsigned) in decimal?",
+        correct_answer: "12",
+        wrong_answer1: "13",
+        wrong_answer2: "14",
+        wrong_answer3: "11"
     },
     {
-        question: "Question 3: ",
-        correct_answer: ""
+        question: "Question 3: What is the number 1100 (twos compliment) in decimal?",
+        correct_answer: "-4",
+        wrong_answer1: "-5",
+        wrong_answer2: "4",
+        wrong_answer3: "-3"
     },
     {
-        question: "Question 4: ",
-        correct_answer: ""
+        question: "Question 4: What is the sum of 1100 + 0011 (unsigned) in decimal?",
+        correct_answer: "15",
+        wrong_answer1: "17",
+        wrong_answer2: "12",
+        wrong_answer3: "13"
     },
     {
-        question: "Question 5: ",
-        correct_answer: ""
+        question: "Question 5: What is the sum of 1100 + 0011 (twos compliment) in decimal?",
+        correct_answer: "-1",
+        wrong_answer1: "-2",
+        wrong_answer2: "2",
+        wrong_answer3: "3"
     }
     
 ]
--- src/app/binary-module/binary-module.component.ts
+++ src/app/binary-module/binary-module.component.ts
@@ -8,12 +8,12 @@ import { binary_quiz_questions } from './binary-module-list';
   templateUrl: 'binary-module.component.html',
   styleUrls: ['binary-module.component.css']
 })
+
 export class BinaryModuleComponent implements OnInit {
   showPractice: boolean;
   showQuiz: boolean;
   currentQuestion: number;
   correctAnswer: number;
-  first: boolean;
   isIncorrect: boolean;
   isCorrect: boolean;
   finishedPractice: boolean;
@@ -21,13 +21,12 @@ export class BinaryModuleComponent implements OnInit {
 
   sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
 
+  // Constructor
   constructor() {
     this.showPractice = false;
     this.showQuiz = false;
     this.currentQuestion = 1;
-    this.changePracticeQuestion("0");
     this.correctAnswer = this.getRandomInt(3);
-    this.first = true;
     this.isIncorrect = false;
     this.isCorrect = false;
     this.finishedPractice = false;
@@ -37,70 +36,243 @@ export class BinaryModuleComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   *  showPractiveProblems() : void
+   * 
+   *  will display practice problems
+   */
   showPracticeProblems() {
     this.showPractice = true;
+    this.showQuiz = false;
+    this.setFirstQuestion();
+    this.currentQuestion = 1;
   }
 
+  /**
+   * showQuizProblems() : void
+   * 
+   * will display quiz problems
+   */
   showQuizProblems() {
     this.showPractice = false;
     this.showQuiz = true;
+    this.setFirstQuizQuestion();
     this.correctAnswer = this.getRandomInt(3);
+    this.currentQuestion = 1;
   }
 
+  /**
+   * changePracticeQuestions(clicked: string) : void
+   * @param clicked - string that what answer button pressed
+   * 
+   * takes user input from practice button questions and decides if question is correct or not and will change question based on that
+   */
   changePracticeQuestion(clicked: string) {
+    // If the selected answer is correct...
     if (this.correctAnswer == parseInt(clicked) && !this.finishedPractice) {
       this.showCorrect();
-      var tochange = document.getElementById("question");
-      if (tochange != null && this.currentQuestion < 5)
-        tochange.innerHTML = binary_practice_questions[this.currentQuestion].question;
-      this.currentQuestion = this.currentQuestion + 1;
-      this.correctAnswer = this.getRandomInt(4);
+      //Update the question element to the next question
+      var questionToChange = document.getElementById("question");
+      if (questionToChange != null && this.currentQuestion <= 5)
+        questionToChange.innerHTML = binary_practice_questions[this.currentQuestion].question;
 
+      // Randomly select the next button to be correct
+      this.correctAnswer = this.getRandomInt(4);
+      var correctButtonToChange = document.getElementById(this.correctAnswer.toString());
+
+      // For loop that changes each button that's not the generated random number to each of the wrong answers in the list
+      var i: number;
+      var wrongAnswers = 0;
+      for(i=1; i<5; i++){
+        if(i!=this.correctAnswer){
+          var wrongButtons = document.getElementById(i.toString())
+          if(wrongButtons !=null){
+            if(wrongAnswers == 0)
+              wrongButtons.innerHTML = binary_practice_questions[this.currentQuestion].wrong_answer1
+            if(wrongAnswers == 1)
+              wrongButtons.innerHTML = binary_practice_questions[this.currentQuestion].wrong_answer2
+            if(wrongAnswers == 2)
+              wrongButtons.innerHTML = binary_practice_questions[this.currentQuestion].wrong_answer3
+          wrongAnswers++;
+          }
+        }
+      }
+
+      // Changes the correct answer button to the provided correct answer
+      if(correctButtonToChange != null)
+        correctButtonToChange.innerHTML = binary_practice_questions[this.currentQuestion].correct_answer;
+
+      // Updates the current question to the next one
+      this.currentQuestion = this.currentQuestion + 1;
+
+      // Checks if the question is greater than the number of questions in the list
       if (this.currentQuestion == 6){
         this.finishedPractice = true;
         this.currentQuestion = 1;
       }
-
     }
-    else if (!this.first && !this.finishedPractice) {
+    // Shows the incorrect answer popup
+    else if (!this.finishedPractice) {
       this.showIncorrect();
     }
-    this.first = false;
+
   }
 
+  /**
+   * changePracticeQuestions(clicked: string) : void
+   * @param clicked - string that what answer button pressed
+   * 
+   * takes user input from quiz button questions and decides if question is correct or not and will change question based on that
+   */
   changeQuizQuestion(clicked: string) {
     if (this.correctAnswer == parseInt(clicked) && !this.finishedQuiz) {
       this.showCorrect();
-      var tochange = document.getElementById("question");
-      if (tochange != null  && this.currentQuestion < 5)
-        tochange.innerHTML = binary_quiz_questions[this.currentQuestion].question;
-      this.currentQuestion = this.currentQuestion + 1;
+      var questionToChange = document.getElementById("question");
+      if (questionToChange != null  && this.currentQuestion < 5)
+        questionToChange.innerHTML = binary_quiz_questions[this.currentQuestion].question;
       this.correctAnswer = this.getRandomInt(4);
+
+      // For loop that changes each button that's not the generated random number to each of the wrong answers in the list
+      var correctButtonToChange = document.getElementById(this.correctAnswer.toString());
+      var i: number;
+      var wrongAnswers = 0;
+      for(i=1; i<5; i++){
+        if(i!=this.correctAnswer){
+          var wrongButtons = document.getElementById(i.toString())
+          if(wrongButtons !=null){
+            if(wrongAnswers == 0)
+              wrongButtons.innerHTML = binary_quiz_questions[this.currentQuestion].wrong_answer1
+            if(wrongAnswers == 1)
+              wrongButtons.innerHTML = binary_quiz_questions[this.currentQuestion].wrong_answer2
+            if(wrongAnswers == 2)
+              wrongButtons.innerHTML = binary_quiz_questions[this.currentQuestion].wrong_answer3
+          wrongAnswers++;
+          }
+        }
+      }
+
+      if(correctButtonToChange != null)
+        correctButtonToChange.innerHTML = binary_quiz_questions[this.currentQuestion].correct_answer;
+
+      this.currentQuestion = this.currentQuestion + 1;
+
 
       if (this.currentQuestion == 6)
         this.finishedQuiz = true;
     }
-    else if (!this.first && !this.finishedQuiz) {
+    else if (!this.finishedQuiz) {
       this.showIncorrect();
     }
-    this.first = false;
+
   }
-  // Used for randomly generating correct answers
+
+  /**
+   * async setFirstQuestion()
+   * 
+   * required because of loading delays
+   * shows the first question when practice questions are opened
+   */
+  async setFirstQuestion(){
+    // Waits 20 miliseconds for the elements to load
+    await this.sleep(20);
+
+    // Gets the element of id "question" and changes it to the first question in the list
+    var question = document.getElementById("question");
+    if(question!=null)
+    question.innerHTML = binary_practice_questions[0].question;
+
+    // Gets the button with the id of the randomly generated correct answer
+    var correctButtonToChange = document.getElementById(this.correctAnswer.toString());
+
+    // For loop that changes each button that's not the generated random number to each of the wrong answers in the list
+    var i: number;
+    var wrongAnswers = 0;
+    for(i=1; i<5; i++){
+      if(i!=this.correctAnswer){
+        var wrongButtons = document.getElementById(i.toString())
+        if(wrongButtons !=null){
+          if(wrongAnswers == 0)
+            wrongButtons.innerHTML = binary_practice_questions[0].wrong_answer1
+          if(wrongAnswers == 1)
+            wrongButtons.innerHTML = binary_practice_questions[0].wrong_answer2
+          if(wrongAnswers == 2)
+            wrongButtons.innerHTML = binary_practice_questions[0].wrong_answer3
+        wrongAnswers++;
+        }
+      }// End of outside if statement
+    }// End of for loop
+
+    // Sets the text of the correct answer button to the correct answer
+    if(correctButtonToChange != null)
+    correctButtonToChange.innerHTML = binary_practice_questions[0].correct_answer;
+  }
+
+  /**
+   * async setFirstQuizQuestion()
+   * 
+   * required because of loading delays
+   * shows the first question when quiz questions are opened
+   */
+  async setFirstQuizQuestion(){
+    await this.sleep(20);
+    var question = document.getElementById("question");
+    if(question!=null)
+    question.innerHTML = binary_quiz_questions[0].question;
+
+    var correctButtonToChange = document.getElementById(this.correctAnswer.toString());
+    var i: number;
+    var wrongAnswers = 0;
+    for(i=1; i<5; i++){
+      if(i!=this.correctAnswer){
+        var wrongButtons = document.getElementById(i.toString())
+        if(wrongButtons !=null){
+          if(wrongAnswers == 0)
+            wrongButtons.innerHTML = binary_quiz_questions[0].wrong_answer1
+          if(wrongAnswers == 1)
+            wrongButtons.innerHTML = binary_quiz_questions[0].wrong_answer2
+          if(wrongAnswers == 2)
+            wrongButtons.innerHTML = binary_quiz_questions[0].wrong_answer3
+        wrongAnswers++;
+        }
+      }// End of outside if statement
+    }// End of for loop
+    if(correctButtonToChange != null)
+    correctButtonToChange.innerHTML = binary_quiz_questions[0].correct_answer;
+  }
+
+  /**
+   * getRandomInt(max: number) : int
+   * 
+   * @param max - the upper bound of random number generator
+   * @returns - a random int within the range of 1-max
+   * 
+   * takes upper bound of random number to generate and will return int within the range of 1-max
+   */
   getRandomInt(max: number) {
     return Math.floor(Math.random() * max) + 1;
   }
 
+  /**
+   * async showIncorrect()
+   * 
+   * will display if the chosen answer is incorrect, then will make it dissapear after 2 seconds
+   */
   async showIncorrect() {
     this.isCorrect = false;
     this.isIncorrect = true;
     await this.sleep(2000);
     this.isIncorrect = false;
   }
+
+  /**
+   * async showCorrect()
+   * 
+   * will display if the chosen answer is correct, then will make it dissapear after 2 seconds
+   */
   async showCorrect() {
     this.isIncorrect = false;
     this.isCorrect = true;
     await this.sleep(2000);
     this.isCorrect = false;
   }
-
 }
